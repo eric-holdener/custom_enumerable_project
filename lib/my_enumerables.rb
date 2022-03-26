765c805d281d0976b39e2f5fd489ecf03424a9c1
EHoldener <eholdener12@gmail.com>
2022-03-25 23:14:04 -0700

    finished my_each_with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -45,6 +45,16 @@ module Enumerable
       length
     end
   end
+
+  def my_each_with_index
+    return to_enum(:my_each) unless block_given?
+    i = 0
+    while i < length
+      yield self[i], i
+      i += 1
+    end
+    self
+  end
 end
 
 # You will first have to define my_each
